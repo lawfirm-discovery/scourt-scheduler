f4ee13657bd250bb5bf5939bb60f92a4810dd63c
ykchoi <doexercise@naver.com>
2025-08-28 10:24:18 +0900

    2025.08.28 - 문법오류 수정

--- app/service/scheduler.py
+++ app/service/scheduler.py
@@ -211,8 +211,8 @@ class SupremeCourtScheduler:
                             "사건번호": case.case_number,
                             "등록건수": len(history),
                             "진행내용": "\n".join(
-                                [
-                                    "",  # 첫줄에 개행문자(\n) 추가하기 위한 용도
+                                ["\n"]  # 첫줄에 개행문자 추가하기 위한 용도
+                                + [
                                     f"{i+1}. {h.date} - {h.content}"
                                     for i, h in enumerate(history)
                                 ]
